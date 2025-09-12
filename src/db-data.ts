8a2865d1b3bf90d2e0c9f0b1c303b21576089134
Alessandro Zagheni <zagheni@gmail.com>
2025-09-11 21:13:11 -0300

    Incluindo campo de impressão

--- src/db-data.ts
+++ src/db-data.ts
@@ -4,6 +4,7 @@ export const ANAMNESES: any = [
     {
         id: 1,
         descricao: "Oi, tudo bem? Neste aplicativo faremos algumas perguntas importantes para o seu atendimento com os enfermeiros e médicos. Você responderá clicando algumas respostas ‘sim’, ‘não’ ou ‘não sei’ e também haverá uma escala de dor que vai de 0 a 10. Essa escala tem rostos e cores que você escolherá informando qual o nível de sua dor.",
+        impressao: "Oi, tudo bem? Neste aplicativo faremos algumas perguntas importantes para o seu atendimento com os enfermeiros e médicos. Você responderá clicando algumas respostas ‘sim’, ‘não’ ou ‘não sei’ e também haverá uma escala de dor que vai de 0 a 10. Essa escala tem rostos e cores que você escolherá informando qual o nível de sua dor.",
         video: "1.mp4",
         inicio: 2
     },
@@ -11,12 +12,14 @@ export const ANAMNESES: any = [
         id: 2,
         isNome: 1,
         descricao: "Modelo de Pergunta aguardando resposta do nome (MANDATÓRIO):",
+        impressao: "Modelo de Pergunta aguardando resposta do nome (MANDATÓRIO):",
         video: "4.mp4",
         texto: 3
     },
     {
         id: 3,
         descricao: "Modelo de Pergunta SIM e NÃO:",
+        impressao: "Modelo de Pergunta SIM e NÃO:",
         video: "2.mp4",
         sim: 4,
         nao: 4
@@ -24,6 +27,7 @@ export const ANAMNESES: any = [
     {
         id: 4,
         descricao: "Modelo de Pergunta SIM, NÃO e NÃO SEI:",
+        impressao: "Modelo de Pergunta SIM, NÃO e NÃO SEI:",
         video: "2.mp4",
         sim: 5,
         naosei: 5,
@@ -32,12 +36,14 @@ export const ANAMNESES: any = [
     {
         id: 5,
         descricao: "Modelo de Pergunta aguardando resposta escrita:",
+        impressao: "Modelo de Pergunta aguardando resposta escrita:",
         video: "4.mp4",
         texto: 6
     },
     {
         id: 6,
         descricao: "Modelo de Pegunta OPÇÕES?",
+        impressao: "Modelo de Pegunta OPÇÕES?",
         video: "5.mp4",
         opcao1: 7,
         opcao1_desc: 'Masculino',
@@ -55,36 +61,42 @@ export const ANAMNESES: any = [
     {
         id: 7,
         descricao: "Modelo de pergunta com resposta no formato data:",
+        impressao: "Modelo de pergunta com resposta no formato data:",
         video: "6.mp4",
         data: 8
     },
     {
         id: 8,
         descricao: "Modelo de Pergunta com resposta numérica:",
+        impressao: "Modelo de Pergunta com resposta numérica:",
         video: "7.mp4",
         numero: 10
     },
     {
         id: 10,
         descricao: "Modelo de Pergunta com resposta CPF:",
+        impressao: "Modelo de Pergunta com resposta CPF:",
         video: "10.mp4",
         cpf: 13
     },
     {
         id: 13,
         descricao: "Modelo de Pergunta com resposta E-mail:",
+        impressao: "Modelo de Pergunta com resposta E-mail:",
         video: "13.mp4",
         email: 14
     },
     {
         id: 14,
         descricao: "Modelo de Pergunta com resposta Telefone:",
+        impressao: "Modelo de Pergunta com resposta Telefone:",
         video: "14.mp4",
         telefone: 15
     },
     {
         id: 15,
         descricao: "Modelo de Pergunta com resposta ESCALA:",
+        impressao: "Modelo de Pergunta com resposta ESCALA:",
         video: "23.mp4",
         escala: 16
     },
@@ -92,6 +104,7 @@ export const ANAMNESES: any = [
         id: 16,
         confidencial: 1,
         descricao: "Modelo de Pergunta aguardando resposta escrita e PRIVADA AO MÉDICO:",
+        impressao: "Modelo de Pergunta aguardando resposta escrita e PRIVADA AO MÉDICO:",
         video: "4.mp4",
         texto: 99999
     },
@@ -99,6 +112,7 @@ export const ANAMNESES: any = [
     {
         id: 99999,
         descricao: "Registro concluido!",
+        impressao: "Registro concluido!",
         video: "99999.mp4",
         fim: 1
     }
